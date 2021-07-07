708e0d8d17fecc62b62ff0a91cdc792620587a52
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2021-07-07 19:23:17 -0300

    Update Funcoes.java
    Assalariado e horistas separados, adicionado cartão de ponto funcional.

--- Funcoes.java
+++ Funcoes.java
@@ -9,86 +9,67 @@ public class Funcoes {
     Scanner input = new Scanner(System.in);
     Menu menu = new Menu();
 
-    public int pagamento(int escolha) {
-        Horista horista = new Horista();
-        Mensal mensal = new Mensal();
+	public void add_horista (int id) {
+		Horista Novo_horista = new Horista();
 
-        while(true) {
-            if(escolha == 1) {
-                System.out.printf("Salario por hora: ");
-                horista.salario = input.nextDouble();
-                input.nextLine();
-
-                return 1;
-            }
-            else if(escolha == 2) {
-                System.out.printf("Salario mensal: ");
-                mensal.salario = input.nextDouble();
-                input.nextLine();
-
-                return 2;
-            }
-            else {
-                menu.alert_escolha_invalida();
-                escolha = input.nextInt();
-                input.nextLine();
-
-                continue;
-            }
-        }
-    }
-
-    public void Add_empregado (int id) {
-        int tipo_pagamento;
-        Horista Novo_horista = new Horista();
-        Mensal Novo_mensal = new Mensal();
-
-        menu.add_tipo_pagamento();
-        tipo_pagamento = input.nextInt();
-        input.nextLine();
-
-        if(tipo_pagamento == 1) { //Horista
-            menu.add_nome();
-            Novo_horista.nome = input.nextLine();
+		menu.add_nome();
+        Novo_horista.nome = input.nextLine();
             
-            menu.add_endereco();
-            Novo_horista.endereco = input.nextLine();
+        menu.add_endereco();
+        Novo_horista.endereco = input.nextLine();
 
-            menu.salario();
-            Novo_horista.salario = input.nextDouble();
-
-            Novo_horista.id = id;
-
-            for (int i = 0; i < 30; i++) {
-                Novo_horista.cartao_do_empregado[0].flag_trabalhou = 1;
-            }
-
-            Horista.add(Novo_horista);
+        menu.salario_horista();
+        Novo_horista.salario = input.nextDouble();
+		
+		Novo_horista.id = id;
+		
+        for (int i = 0; i < 30; i++) {
+            Novo_horista.cartao_do_empregado[i] = new Cartao_de_ponto();
         }
-        else if(tipo_pagamento == 2) { //Mensal
-            menu.add_nome();
-            Novo_mensal.nome = input.nextLine();
-            
-            menu.add_endereco();
-            Novo_mensal.endereco = input.nextLine();
+		
+		Horista.add(Novo_horista);
+	}
+	
+	public void add_assalariado (int id) {
+		Mensal Novo_mensal = new Mensal();
+		
+		menu.add_nome();
+        Novo_mensal.nome = input.nextLine();
 
-            menu.salario();
-            Novo_mensal.salario = input.nextDouble();
+        menu.add_endereco();
+        Novo_mensal.endereco = input.nextLine();
 
-            menu.ver_comissao();
-            Novo_mensal.ver_comissao = input.nextInt();
-            input.nextLine();
-
-            if(Novo_mensal.ver_comissao == 1) {
-                menu.add_comissao();
-                Novo_mensal.comissao = input.nextDouble();
-            }
-
-            Novo_mensal.id = id;
-
-            Mensal.add(Novo_mensal);
-        }
-    }
+        menu.salario_mensal();
+        Novo_mensal.salario = input.nextDouble();
+		
+		Novo_mensal.id = id;
+		
+		menu.tipo_pagamento();
+		
+		menu.ver_comissao();
+		Novo_mensal.ver_comissao = input.nextInt();
+		input.nextLine();
+		
+		if(Novo_mensal.ver_comissao == 1) {
+			menu.porcentagem_comissao();
+			Novo_mensal.comissao = input.nextDouble();
+		}
+		
+		menu.ver_sindicato();
+		Novo_mensal.ver_sindicato = input.nextInt();
+		input.nextLine();
+		
+		if(Novo_mensal.ver_sindicato == 1) {
+			menu.taxa_sindicato();
+			Novo_mensal.taxa_sindicato = input.nextDouble();
+		}
+		
+		menu.tipo_pagamento();
+		Novo_mensal.tipo_pagamento = input.nextInt();
+		input.nextLine();
+		
+		Mensal.add(Novo_mensal);
+	}
 
     public void Lista_de_empregados () {
         System.out.println("Horistas:");
@@ -96,8 +77,8 @@ public class Funcoes {
             Horista aux = Horista.get(i);
             System.out.printf("%s(%d)\n", aux.nome, aux.id);
             for (int j = 0; j < 30; j++) {
-                if(aux.cartao_do_empregado[j].flag_trabalhou != 1) {
-                    System.out.printf("dia %d  :  %dh", j, aux.cartao_do_empregado[j].saida - aux.cartao_do_empregado[j].entrada);
+                if(aux.cartao_do_empregado[j].flag_trabalhou == 1) {
+                    System.out.printf("dia %d  :  %dh\n", j + 1, aux.cartao_do_empregado[j].saida - aux.cartao_do_empregado[j].entrada);
                 }
             }
         }
@@ -121,40 +102,40 @@ public class Funcoes {
                 menu.empregado_removido();
             }
         }
+		for (int i = 0; i < Mensal.size(); i++) {
+            Mensal aux = Mensal.get(i);
+
+            if(aux.id == id) {
+                Mensal.remove(i);
+                menu.empregado_removido();
+            }
+        }
     }
 
-    public void cartao_de_ponto(int id) {
+    public void cartao_de_ponto(int id, int dia) {
+        menu.inOut();
+        int escolha = input.nextInt();
+        input.nextLine();
+
         for (int i = 0; i < Horista.size(); i++) {
-            Horista empregado = Horista.get(i);
+            Horista aux = Horista.get(i);
 
-            if(empregado.id == id) {
-                menu.menu_carto_de_ponto();
-                int escolha_cartao = input.nextInt();
-                input.nextLine();
-
-                if(escolha_cartao == 1) {
+            if(aux.id == id) {
+                if(escolha == 1) {
                     menu.carto_de_ponto_chegada();
-                    for (int j = 0; j < 30; j++) {
-                        if(empregado.cartao_do_empregado[j].flag_trabalhou == 0) {
-                            empregado.cartao_do_empregado[j].entrada = input.nextInt();
-                            input.nextLine();
-
-                            break;
-                        }
-                    }
+                    int horarioChegada = input.nextInt();
+                    input.nextLine();
+                    aux.cartao_do_empregado[dia].entrada = horarioChegada;
                 }
-                else if(escolha_cartao == 2) {
+                else if(escolha == 2) {
                     menu.carto_de_ponto_saida();
-                    for (int j = 0; j < 30; j++) {
-                        if(empregado.cartao_do_empregado[j].flag_trabalhou == 0) {
-                            empregado.cartao_do_empregado[j].saida = input.nextInt();
-                            input.nextLine();
-
-                            empregado.cartao_do_empregado[j].flag_trabalhou = 1;
-
-                            break;
-                        }
-                    }
+                    int horarioSaida = input.nextInt();
+                    input.nextLine();
+                    aux.cartao_do_empregado[dia].saida = horarioSaida;
+                    aux.cartao_do_empregado[dia].flag_trabalhou = 1;
+                }
+                else {
+                    System.out.println("Casa");
                 }
             }
         }
