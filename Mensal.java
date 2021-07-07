87bfc7cde93b0c1570136b69f6cbe935a7a5c3cf
Leonardo Zaia <57048196+leozaia-h@users.noreply.github.com>
2021-07-07 19:24:56 -0300

    Update Mensal.java
    Novas variáveis.

--- Mensal.java
+++ Mensal.java
@@ -2,9 +2,12 @@ public class Mensal {
     String nome;
     String endereco;
     double salario;
+    int tipo_pagamento;
     int ver_comissao;
     double comissao;
-    
-    
+    double valor_vendas;
+	int ver_sindicato;
+	double taxa_sindicato;
+	double taxa_servico;
     int id;
 }
